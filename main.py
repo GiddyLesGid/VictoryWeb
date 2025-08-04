fc579de7dc356a2d362ab0046c0df155b60a00a0
GiddyLesGid <112728731+GiddyLesGid@users.noreply.github.com>
2025-08-04 10:51:53 +0000

    fine tune wsgi

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from app import app
-from vercel_wsgi import handle_wsgi_app
 
-# Export a WSGI handler as expected by Vercel
-handler = handle_wsgi_app(app)
+if __name__ == "__main__":
+    app.run()
 
